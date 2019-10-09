d609792320f043cf2bd5bc286aa012a27065081f
ztstroud <ztstroud@gmail.com>
2019-10-09 16:00:07 -0600

    Move perceptron logic into Perceptron class
    Prediction now takes a full data set, including labels which are ignored

--- ml/perceptron.py
+++ ml/perceptron.py
@@ -2,36 +2,63 @@ import numpy as np
 import random
 
 
-def train(data, *, learning_rate = 1, decay_learning_rate = False, averaged = True, epochs = 10):
+class Perceptron:
+    def train(self, data, *, learning_rate = 1, decay_learning_rate = False, averaged = True, epochs = 10):
+        """
+        Trains a perceptron using the given data.
+
+        :param data: the data to use in training
+        :param learning_rate: the learning rate
+        :param decay_learning_rate: whether or not to decay the learning rate with each epoch
+        :param averaged: return the average of all weights
+        :param epochs: the number of epochs to run
+        """
+
+        num_examples, num_features = data[0].shape
+
+        weights = np.array([[random.uniform(-0.01, 0.01) for _ in range(num_features)]])
+        total_weights = np.array([[0.0 for _ in range(num_features)]])
+
+        for epoch in range(epochs):
+            for example, label in enumerate_data(data):
+                prediction = predict(example, weights)
+
+                if prediction != label:
+                    weights += learning_rate / (1 + epoch) * label * example
+
+                if averaged:
+                    total_weights += weights
+
+        if averaged:
+            self.weights = total_weights / (num_examples * epochs)
+
+        self.weights = weights
+
+    def predict(self, data):
+        """
+        Make a prediction for each of the given examples.
+
+        :param examples: the examples to predict
+
+        :return: predictions for each example
+        """
+
+        print(self.weights)
+        print(self.weights.shape)
+        return predict(data[0], self.weights)
+
+
+def predict(example, weights):
     """
-    Trains a perceptron using the given data.
+    Makes a prediction for each of the given examples.
 
-    :param data: the data to use in training
-    :param learning_rate: the learning rate
-    :param decay_learning_rate: whether or not to decay the learning rate with each epoch
-    :param averaged: return the average of all weights
-    :param epochs: the number of epochs to run
+    :param example: the examples to predict
+    :param weights: the weights to use in the prediction
+
+    :return: predictions for each example
     """
 
-    num_examples, num_features = data[0].shape
-
-    weights = np.array([[random.uniform(-0.01, 0.01) for _ in range(num_features)]])
-    total_weights = np.array([[0.0 for _ in range(num_features)]])
-
-    for epoch in range(epochs):
-        for example, label in enumerate_data(data):
-            prediction = predict(example, weights)
-
-            if prediction != label:
-                weights += learning_rate / (1 + epoch) * label * example
-
-            if averaged:
-                total_weights += weights
-
-    if averaged:
-        return total_weights / (num_examples * epochs)
-
-    return weights
+    return np.sign(example * weights.T)
 
 def enumerate_data(data):
     """
@@ -47,16 +74,3 @@ def enumerate_data(data):
 
     for index in indices:
         yield x[index, :], y[index]
-
-def predict(example, weights):
-    """
-    Makes a prediction for each of the given examples.
-
-    :param example: the examples to predict
-    :param weights: the weights to use in the prediction
-
-    :return: predictions for each example
-    """
-
-    return np.sign((example * weights.T))
-
