703153d2b9ffb9923b6ccfcdee9241ad3e395586
ztstroud <ztstroud@gmail.com>
2019-10-09 16:42:44 -0600

    Add support for folding tuples and numpy arrays

--- cross_validation/folds.py
+++ cross_validation/folds.py
@@ -1,6 +1,8 @@
 import copy
+import numpy as np
 import pandas as pd
 import random
+import scipy.sparse.csr
 
 
 def create_folds(data, count):
@@ -16,8 +18,8 @@ def create_folds(data, count):
     if count < 1:
         raise Exception("You cannot have less than one fold")
 
-    if count > len(data):
-        raise Exception("You cannot have more folds than examples")
+    if isinstance(data, tuple):
+        return _create_folds_tuple(data, count)
 
     if isinstance(data, list):
         return _create_folds_list(data, count)
@@ -25,7 +27,26 @@ def create_folds(data, count):
     if isinstance(data, pd.DataFrame):
         return _create_folds_pandas(data, count)
 
+    if isinstance(data, np.ndarray) or isinstance(data, scipy.sparse.csr.csr_matrix):
+        return _create_folds_numpy(data, count)
+
     raise Exception(f"Unrecognized data type: {type(data)}")
+
+def _create_folds_tuple(data, count):
+    """
+    Create folds from the given data.
+
+    Folds will be created from each element of the tuple, and returned as a list
+    of tuples.
+
+    :param data: the data to fold
+    :param count: the number of folds to create
+
+    :return: a list of folds
+    """
+
+    subfolds = [create_folds(subdata, count) for subdata in data]
+    return [fold for fold in zip(*subfolds)]
 
 def _create_folds_list(data, count):
     """
@@ -51,7 +72,7 @@ def _create_folds_list(data, count):
 
 def _create_folds_pandas(data, count):
     """
-    Creates a fold from the given data
+    Creates folds from the given data
 
     :param data: the data to fold
     :param count: the number of folds to create
@@ -67,6 +88,28 @@ def _create_folds_pandas(data, count):
         high = int((fold_index + 1) * fold_count)
 
         fold = data.iloc[low:high]
+        folds.append(fold)
+
+    return folds
+
+def _create_folds_numpy(data, count):
+    """
+    Creates folds from the given data
+
+    :param data: the data to fold
+    :param count: the number of folds to create
+
+    :return: a list of folds
+    """
+
+    fold_count = data.shape[0] / count
+    folds = list()
+
+    for fold_index in range(count):
+        low = int(fold_index * fold_count)
+        high = int((fold_index + 1) * fold_count)
+
+        fold = data[low:high, :]
         folds.append(fold)
 
     return folds
